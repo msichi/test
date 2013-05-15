250f7a771b27a937f5a9f8d54ebd9dcb39f6a7d7
ickw <mnb.ichi@gmail.com>
2013-05-15 14:11:32 +0900

    3rd

--- test.cpp
+++ test.cpp
@@ -5,7 +5,10 @@ int main(int argc, char *argv[])
   printf("Hellow World\n");
   printf("Hellow World\n");
   printf("Hellow World\n");
-  printf("Hellow Japan\n");
+  printf("Hellow World\n");
+  printf("Hellow World\n");
+  printf("Hellow World\n");
+  printf("Hellow Africa\n");
   printf("Hellow World\n");
   printf("Hellow World\n");
 }
